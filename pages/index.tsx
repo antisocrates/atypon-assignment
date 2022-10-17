4130ac0a9955b66281e5b71dbfc88b0630aeb735
George Karalis <gkaralis@atypon.com>
2022-10-17 20:04:08 +0300

    fix: add fonts to document

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,10 +7,6 @@ const Home: NextPage = () => {
       <Head>
         <title>Atypon's frontend engineering assignment</title>
         <meta name="description" content="Atypon's frontend engineering assignment" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet" />
       </Head>
 
       <main>
