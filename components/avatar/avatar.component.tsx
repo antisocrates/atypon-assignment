54538126fe1368b66f0489d20abebfadb8c397f9
antisocrates <antisocrates@gmail.com>
2022-10-19 11:02:30 +0300

    avatar

---
+++ components/avatar/avatar.component.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from './avatar.module.css';
+import Image from 'next/future/image';
+
+
+export enum AvatarSizes {
+    small,
+    large
+}
+
+export interface AvatarProps {
+    avatarSize?: AvatarSizes;
+    avatarText: string;
+    photoURL?: any;
+}
+
+export const Avatar: React.FC<AvatarProps> = ({avatarSize, avatarText, photoURL}) => {
+    return (
+        <div className={`${styles.avatar} ${avatarSize === AvatarSizes.small ? styles.small : styles.large}`}>
+            {photoURL ?
+                <Image src={photoURL} alt={"user avatar"} className={styles.large}/>
+                : avatarText[0]
+            }
+        </div>
+        // <Image src={photoURL} alt={"user avatar"}/>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,9 @@ import Head from 'next/head'
 import {ActionButton, ActionButtonTypes} from "../components/action-button/action-button.component";
 import {Link} from "../components/icons/Link";
 import {LinkComponent} from "../components/link/link.component";
+import {Avatar, AvatarSizes} from "../components/avatar/avatar.component";
+import avatar2 from "../public/avatar-2.jpg";
+import Image from 'next/future/image'
 
 const Home: NextPage = () => {
     return (
@@ -15,6 +18,7 @@ const Home: NextPage = () => {
             <main>
                 <div className={"container"}>
                     <h1>Components Showcase</h1>
+
                     <h3>Action Buttons:</h3>
                     <ActionButton buttonText={"Action Button"} buttonType={ActionButtonTypes.flat} icon={<Link/>} action={() => {}}/>
                     <ActionButton buttonText={"Action Button"} buttonType={ActionButtonTypes.flatWithIcon} icon={<Link/>} action={() => {}}/>
@@ -22,6 +26,10 @@ const Home: NextPage = () => {
 
                     <h3>Links:</h3>
                     <LinkComponent href={"https://www.atypon.com/"} linkText={"Add description"}/>
+
+                    <h3>Avatars:</h3>
+                    <Avatar avatarSize={AvatarSizes.large} avatarText={"Socrates"} photoURL={avatar2}/>
+
                 </div>
             </main>
         </div>
