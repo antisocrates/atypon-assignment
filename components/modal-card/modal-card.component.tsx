0442dec809f93250160007f623f1beeb07f3a253
antisocrates <antisocrates@gmail.com>
2022-10-21 06:45:15 +0300

    modal-card

--- components/modal-card/modal-card.component.tsx
+++ components/modal-card/modal-card.component.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './modal-card.module.css';
 import {ActionButton, ActionButtonTypes} from "../action-button/action-button.component";
-import {Close, Link} from "../icons";
+import {Close, Copy, Delete, Link} from "../icons";
 
 export interface ModalCardProps {
     avatar: React.ReactNode;
@@ -12,11 +12,18 @@ export const ModalCard: React.FC<ModalCardProps> = ({avatar,placeholder}) => {
     return (
         <div className={`${styles.modalCard} default-box-shadow`}>
             <div className={styles.modalButtonGroup}>
-                <ActionButton buttonText={"Action Button"} buttonType={ActionButtonTypes.flat} icon={<Link/>} action={() => {}}/>
-                <ActionButton buttonText={"Action Button"} buttonType={ActionButtonTypes.flatWithIcon} icon={<Link/>} action={() => {}}/>
-                <ActionButton buttonText={"Action Button"} buttonType={ActionButtonTypes.iconOnly} icon={<Link/>} action={() => {}}/>
-                <i className={"icon"} style={{marginLeft: "auto"}}><Close/></i>
+                <ActionButton buttonText={"Copy Link"} buttonType={ActionButtonTypes.flatWithIcon} icon={<Link/>} action={() => {}}/>
+                <ActionButton buttonText={"Duplicate"} buttonType={ActionButtonTypes.flatWithIcon} icon={<Copy/>} action={() => {}}/>
+                <ActionButton buttonText={"Delete"} buttonType={ActionButtonTypes.flatWithIcon} icon={<Delete/>} action={() => {}}/>
+
+                <i style={{marginLeft: "auto"}}>
+                    <ActionButton buttonText={"Action Button"} buttonType={ActionButtonTypes.iconOnly} icon={<Close/>} action={() => {}}/>
+                </i>
             </div>
+            <div className={styles.modalInfo}>
+                <h1 className={"text-title"}>Content Prep</h1>
+            </div>
+
         </div>
     )
 }
